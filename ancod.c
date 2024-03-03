73aa62768a368aaa47800ff9ce55ad8a5ffbc93b
Guilherme Puida <guilherme@puida.xyz>
2024-03-03 01:32:41 -0300

    print first argument in green

--- ancod.c
+++ ancod.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void
+ansi_code_for(int value)
+{
+    printf("\033[%dm", value);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -9,6 +15,8 @@ main(int argc, char **argv)
     }
 
     char *input = argv[1];
+
+    ansi_code_for(32);
     puts(input);
 
     exit(EXIT_SUCCESS);
